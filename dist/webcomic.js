774c95be23ff06edfddf22e916566a310d9c65b8
NilsPosthumus <nils.posthumus@ksb-sg.ch>
2021-06-23 22:10:18 +0200

    add transition fade and slide-left; fixed some other things

--- dist/webcomic.js
+++ dist/webcomic.js
@@ -1,7 +1,27 @@
 var webcomic;
 (function (webcomic) {
+    var Animations;
+    (function (Animations) {
+        Animations[Animations["FadeIn"] = 0] = "FadeIn";
+    })(Animations = webcomic.Animations || (webcomic.Animations = {}));
     function initialize() {
+        console.log("initialized");
+        var webComicContainers = document.getElementsByClassName("webcomic");
+        Array.prototype.forEach.call(webComicContainers, (function (webComicContainer) {
+            var panels = webComicContainer.children;
+            panels = Array.prototype.filter.call(panels, function (element) {
+                return element.className.includes("panel");
+            });
+            document.body.addEventListener("click", function () {
+                fadeIn(panels[2]);
+            });
+            console.log(panels);
+        }));
     }
     webcomic.initialize = initialize;
+    function fadeIn(e) {
+        e.style.animation = "slide-left .5s ease-in-out";
+        e.style.opacity = "1";
+    }
+    window.onload = initialize;
 })(webcomic || (webcomic = {}));
-webcomic.initialize();
--- ts/webcomic.ts
+++ ts/webcomic.ts
@@ -1,8 +1,36 @@
 namespace webcomic {
 
-    export function initialize() {
+    export enum Animations {
+        FadeIn
     }
 
-}
+    export function initialize() {
 
-webcomic.initialize();
+        console.log("initialized");
+
+        const webComicContainers : HTMLCollection = document.getElementsByClassName("webcomic");
+
+        Array.prototype.forEach.call(webComicContainers, (webComicContainer  => {
+            let panels : HTMLCollection = webComicContainer.children;
+            panels = Array.prototype.filter.call(panels, (element) => 
+                element.className.includes("panel")
+            );
+
+            document.body.addEventListener("click", () => {
+                fadeIn(panels[2] as HTMLElement);
+            })
+
+            console.log(panels);
+        }))
+
+
+    }
+
+    function fadeIn(e:HTMLElement) {
+        e.style.animation="slide-left .5s ease-in-out";
+        e.style.opacity="1";
+
+    }
+
+    window.onload = initialize;
+}
