1b62316f4bf22abce6e95587b5946e6b8302bfa1
NilsPosthumus <nils.posthumus@ksb-sg.ch>
2021-06-24 20:23:08 +0200

    new animations and some refactoring

--- dist/webcomic.js
+++ dist/webcomic.js
@@ -1,27 +1,78 @@
 var webcomic;
 (function (webcomic) {
-    var Animations;
-    (function (Animations) {
-        Animations[Animations["FadeIn"] = 0] = "FadeIn";
-    })(Animations = webcomic.Animations || (webcomic.Animations = {}));
+    ;
+    var animations = [
+        { "class": "intro-fade", name: "fadeIn", duration: 0.5, mode: "forwards", easing: "ease-in-out", preparation: function (e) { return e.style.opacity = '0'; } },
+        { "class": "intro-curtain-horizontal", name: "curtain-horizontal", duration: 0.5, mode: "forwards", easing: "ease-in-out", preparation: function (e) { return e.style.transform = "scaleY(0)"; } },
+        { "class": "intro-curtain-vertical", name: "curtain-vertical", duration: 0.5, mode: "forwards", easing: "ease-in-out", preparation: function (e) { return e.style.transform = "scaleY(0)"; } },
+        { "class": "intro-slide-left", name: "slide-left", duration: 0.5, mode: "forwards", easing: "ease-in-out", preparation: function (e) { e.style.clipPath = "inset(0 0 0 100%)"; } },
+        { "class": "intro-slide-right", name: "slide-right", duration: 0.5, mode: "forwards", easing: "ease-in-out", preparation: function (e) { e.style.clipPath = "inset(0 100% 0 0)"; } },
+        { "class": "intro-slide-top", name: "slide-top", duration: 0.5, mode: "forwards", easing: "ease-in-out", preparation: function (e) { e.style.clipPath = "inset(100% 0 0 0)"; } },
+        { "class": "intro-slide-bottom", name: "slide-bottom", duration: 0.5, mode: "forwards", easing: "ease-in-out", preparation: function (e) { e.style.clipPath = "inset(0 0 100% 0)"; } },
+        { "class": "intro-swipe-left", name: "swipe-left", duration: 0.5, mode: "forwards", easing: "ease-in-out", preparation: function (e) { e.style.clipPath = "inset(0 100% 0 0)"; } },
+        { "class": "intro-swipe-right", name: "swipe-right", duration: 0.5, mode: "forwards", easing: "ease-in-out", preparation: function (e) { e.style.clipPath = "inset(0 0 0 100%)"; } },
+        { "class": "intro-swipe-top", name: "swipe-top", duration: 0.5, mode: "forwards", easing: "ease-in-out", preparation: function (e) { e.style.clipPath = "inset(0 0 100% 0)"; } },
+        { "class": "intro-swipe-bottom", name: "swipe-bottom", duration: 0.5, mode: "forwards", easing: "ease-in-out", preparation: function (e) { e.style.clipPath = "inset(100% 0 0 0)"; } },
+        { "class": "intro-grow", name: "grow", duration: 0.5, mode: "forwards", easing: "ease-in-out", preparation: function (e) { return e.style.transform = "scale(0)"; } }
+    ];
+    var AnimationManager = /** @class */ (function () {
+        function AnimationManager() {
+            this.panels = new Array();
+            this.anims = new Array();
+            this.index = 0;
+            this.lastIndex = 0;
+        }
+        AnimationManager.prototype.restart = function () {
+            var _this = this;
+            this.index = 0;
+            this.lastIndex = this.panels.length;
+            this.panels.forEach(function (panel, i) { _this.anims[i].preparation(panel); panel.style.animation = ""; });
+        };
+        AnimationManager.prototype.nextPanel = function () {
+            if (this.index == this.lastIndex) {
+                if (webcomic.onNextPage)
+                    webcomic.onNextPage();
+                else
+                    this.restart();
+                return;
+            }
+            if (!this.panels[this.index] && !this.anims[this.index])
+                return;
+            if (webcomic.onNextPanel)
+                this.index > 0 ? webcomic.onNextPanel(this.panels[this.index - 1], this.panels[this.index]) : webcomic.onNextPanel(this.panels[this.index], this.panels[this.index]);
+            playAnim(this.panels[this.index], this.anims[this.index]);
+            this.index++;
+        };
+        return AnimationManager;
+    }());
     function initialize() {
         console.log("initialized");
         var webComicContainers = document.getElementsByClassName("webcomic");
         Array.prototype.forEach.call(webComicContainers, (function (webComicContainer) {
+            var animManager = new AnimationManager();
             var panels = webComicContainer.children;
             panels = Array.prototype.filter.call(panels, function (element) {
                 return element.className.includes("panel");
             });
+            Array.prototype.forEach.call(panels, function (element) {
+                animations.forEach(function (animation) {
+                    if (element.className.includes(animation["class"])) {
+                        animManager.panels.push(element);
+                        animManager.anims.push(animation);
+                    }
+                    ;
+                });
+            });
+            animManager.restart();
             document.body.addEventListener("click", function () {
-                fadeIn(panels[2]);
+                animManager.nextPanel();
             });
             console.log(panels);
         }));
     }
     webcomic.initialize = initialize;
-    function fadeIn(e) {
-        e.style.animation = "slide-left .5s ease-in-out";
-        e.style.opacity = "1";
+    function playAnim(target, animation) {
+        target.style.animation = animation.duration + "s " + animation.easing + " " + animation.mode + " " + animation.name;
     }
     window.onload = initialize;
 })(webcomic || (webcomic = {}));
--- ts/webcomic.ts
+++ ts/webcomic.ts
@@ -1,7 +1,50 @@
 namespace webcomic {
 
-    export enum Animations {
-        FadeIn
+    export let onNextPage : () => void;
+    export let onNextPanel : (previousPanel: HTMLElement, nextPanel: HTMLElement) => void;
+
+    interface Animation {class : string, name : string, duration : number, mode: FillMode, easing : string, preparation: (e: HTMLElement) => void};
+    const animations : Animation[] = [
+        {class: "intro-fade", name: "fadeIn", duration: 0.5, mode: "forwards", easing: "ease-in-out", preparation: (e) => e.style.opacity = '0'},
+        {class: "intro-curtain-horizontal", name: "curtain-horizontal", duration: 0.5, mode: "forwards", easing: "ease-in-out", preparation: (e) => e.style.transform = "scaleY(0)"},
+        {class: "intro-curtain-vertical", name: "curtain-vertical", duration: 0.5, mode: "forwards", easing: "ease-in-out", preparation: (e) => e.style.transform = "scaleY(0)"},
+        {class: "intro-slide-left", name: "slide-left", duration: 0.5, mode: "forwards", easing: "ease-in-out", preparation: (e) =>  {e.style.clipPath = "inset(0 0 0 100%)"}},
+        {class: "intro-slide-right", name: "slide-right", duration: 0.5, mode: "forwards", easing: "ease-in-out", preparation: (e) =>  {e.style.clipPath = "inset(0 100% 0 0)"}},
+        {class: "intro-slide-top", name: "slide-top", duration: 0.5, mode: "forwards", easing: "ease-in-out", preparation: (e) =>  {e.style.clipPath = "inset(100% 0 0 0)"}},
+        {class: "intro-slide-bottom", name: "slide-bottom", duration: 0.5, mode: "forwards", easing: "ease-in-out", preparation: (e) =>  {e.style.clipPath = "inset(0 0 100% 0)"}},
+        {class: "intro-swipe-left", name: "swipe-left", duration: 0.5, mode: "forwards", easing: "ease-in-out", preparation: (e) =>  {e.style.clipPath = "inset(0 100% 0 0)"}},
+        {class: "intro-swipe-right", name: "swipe-right", duration: 0.5, mode: "forwards", easing: "ease-in-out", preparation: (e) =>  {e.style.clipPath = "inset(0 0 0 100%)"}},
+        {class: "intro-swipe-top", name: "swipe-top", duration: 0.5, mode: "forwards", easing: "ease-in-out", preparation: (e) =>  {e.style.clipPath = "inset(0 0 100% 0)"}},
+        {class: "intro-swipe-bottom", name: "swipe-bottom", duration: 0.5, mode: "forwards", easing: "ease-in-out", preparation: (e) =>  {e.style.clipPath = "inset(100% 0 0 0)"}},
+        {class: "intro-grow", name: "grow", duration: 0.5, mode: "forwards", easing: "ease-in-out", preparation: (e) => e.style.transform = "scale(0)"}
+    ];
+
+    class AnimationManager {
+        panels: HTMLElement[] = new Array();
+        anims: Animation[] = new Array();
+        index: number = 0;
+        lastIndex: number = 0;
+
+        restart () {
+            this.index = 0;
+            this.lastIndex = this.panels.length;
+            this.panels.forEach((panel, i) => {this.anims[i].preparation(panel); panel.style.animation = ""});
+        }
+
+        nextPanel () {
+            if (this.index == this.lastIndex) {
+                if (onNextPage)
+                    onNextPage();
+                else
+                    this.restart();
+                return;
+            }
+            if (!this.panels[this.index] && !this.anims[this.index]) return;
+            if (onNextPanel)
+                this.index > 0 ? onNextPanel(this.panels[this.index - 1], this.panels[this.index]) : onNextPanel(this.panels[this.index], this.panels[this.index]);
+            playAnim(this.panels[this.index], this.anims[this.index])
+            this.index++;
+        }
     }
 
     export function initialize() {
@@ -11,13 +54,26 @@ namespace webcomic {
         const webComicContainers : HTMLCollection = document.getElementsByClassName("webcomic");
 
         Array.prototype.forEach.call(webComicContainers, (webComicContainer  => {
+
+            const animManager = new AnimationManager();
+
             let panels : HTMLCollection = webComicContainer.children;
-            panels = Array.prototype.filter.call(panels, (element) => 
-                element.className.includes("panel")
-            );
+            panels = Array.prototype.filter.call(panels, (element) => {
+                    return element.className.includes("panel");
+            });
+
+            Array.prototype.forEach.call(panels, (element) => {
+                animations.forEach((animation) => {
+                    if( element.className.includes(animation.class)) {
+                        animManager.panels.push(element);
+                        animManager.anims.push(animation);
+                    };
+                } )
+            });
+            animManager.restart();
 
             document.body.addEventListener("click", () => {
-                fadeIn(panels[2] as HTMLElement);
+                animManager.nextPanel();
             })
 
             console.log(panels);
@@ -26,10 +82,8 @@ namespace webcomic {
 
     }
 
-    function fadeIn(e:HTMLElement) {
-        e.style.animation="slide-left .5s ease-in-out";
-        e.style.opacity="1";
-
+    function playAnim(target:HTMLElement, animation:Animation) {
+        target.style.animation = `${animation.duration}s ${animation.easing} ${animation.mode} ${animation.name}`;
     }
 
     window.onload = initialize;
